9fb58598113cf46276ce47e15656ba69d09e85e2
diabloevagto <diabloevagto@gmail.com>
2020-07-12 22:53:43 +0800

    fixed: env

--- .env-rc.js
+++ .env-rc.js
@@ -3,16 +3,16 @@ const baseName = 'space-f2e-calculator';
 module.exports = {
   development: {
     REACT_APP_BASE_NAME: baseName,
-    PUBLIC_URL: `http://localhost:3000/${baseName}`,
+    // PUBLIC_URL: `http://localhost:3000/${baseName}`,
   },
   test: {
     GENERATE_SOURCEMAP: false,
     REACT_APP_BASE_NAME: baseName,
-    PUBLIC_URL: `https://diabloevagto.github.io/${baseName}`,
+    // PUBLIC_URL: `https://diabloevagto.github.io/${baseName}`,
   },
   production: {
     GENERATE_SOURCEMAP: false,
     REACT_APP_BASE_NAME: baseName,
-    PUBLIC_URL: `https://diabloevagto.github.io/${baseName}`,
+    // PUBLIC_URL: `https://diabloevagto.github.io/${baseName}`,
   },
 };
